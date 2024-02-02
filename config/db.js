0d3734442ef132f0d098cc1e8a2973f83a67b376
Jaber kh <97162517+Jabkh@users.noreply.github.com>
2024-02-02 09:28:49 +0100

    Update db.js variable d'environnement

--- config/db.js
+++ config/db.js
@@ -1,10 +1,20 @@
+
 const mysql = require('mysql2');
+require('dotenv').config(); // Charge les variables d'environnement depuis le fichier .env
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'votre_utilisateur',
-  password: 'votre_mot_de_passe',
-  database: 'bibliotheque'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données :', err);
+  } else {
+    console.log('Connecté à la base de données MySQL');
+  }
 });
 
 module.exports = connection;
